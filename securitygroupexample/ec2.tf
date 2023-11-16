d97549949eea87fa307f5c7ea267d32e060d2a97
hemasri15 <hemasree3211@gmail.com>
2023-11-16 10:40:55 +0530

    security group example with provisioners

---
+++ securitygroupexample/ec2.tf
@@ -0,0 +1,52 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+resource "aws_instance" "terraformdemo" {
+  ami                    = var.ami_id
+  instance_type          = var.instance_type
+  key_name               = "ssh"
+  vpc_security_group_ids = [aws_security_group.http_access.id]
+
+connection {
+  type        = "ssh"
+  user        = "ec2-user"
+  private_key = "${file("ssh.pem")}"
+  host = self.public_ip
+}
+provisioner "remote-exec"{
+  inline = ["sudo yum install -y nginx","sudo service nginx start"]
+
+
+}
+}
+resource "aws_security_group" "http_access" {
+  name = "http_access"
+
+
+  ingress {
+    from_port   = "22"
+    to_port     = "22"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = "80"
+    to_port     = "80"
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  egress {
+    from_port   = "0"
+    to_port     = "0"
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+
+
+
+
+
+
---
+++ securitygroupexample/variable.tf
@@ -0,0 +1,15 @@
+variable "ami_id" {
+  type    = string
+  default = "ami-05c13eab67c5d8861"
+
+}
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+
+}
+
+
+
+
+
