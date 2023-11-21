d79d271d4622378ba05ca72b23f2d24947e1cc84
hemasri15 <hemasree3211@gmail.com>
2023-11-21 10:22:43 +0530

    commit

--- loadbalancerexamples/targetgroup.tf
+++ loadbalancerexamples/targetgroup.tf
@@ -4,12 +4,5 @@ resource "aws_lb_target_group" "sample_tg" {
    port               = 80
    protocol           = "HTTP"
    
-   health_check {
-      healthy_threshold   = var.health_check["healthy_threshold"]
-      interval            = var.health_check["interval"]
-      unhealthy_threshold = var.health_check["unhealthy_threshold"]
-      timeout             = var.health_check["timeout"]
-      path                = var.health_check["path"]
-      port                = var.health_check["port"]
-  }
+   
 }
